bf22c90e729c6a0f33dbe3c9209db816dde9fa12
Nicholas izosimov <nicholasizosimov@Nicholass-MacBook-Pro.local>
2022-11-10 16:38:37 +0100

    Create Navigation & Layout

---
+++ taxegator/components/NavOptions.js
@@ -0,0 +1,53 @@
+import { FlatList, Text, TouchableOpacity, View, Image } from 'react-native';
+import tw from 'twrnc';
+import React from 'react';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+
+const data = [
+    {
+        id: "123",
+        title: "Get a ride",
+        image: "https://links.papareact.com/3pn",
+        screen: "MapScreen",
+    },
+    {
+        id: "456",
+        title: "Travel history",
+        image: "https://assets-global.website-files.com/6050a76fa6a633d5d54ae714/609147088669907f652110b0_report-an-issue(about-maps).jpeg",
+        screen: "TravelHistoryScreen",
+    }
+]
+
+const NavOptions = () => {
+    const navigation = useNavigation();
+    return (
+        <FlatList
+            data={data}
+            horizontal
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+                <TouchableOpacity
+                onPress={() => navigation.navigate(item.screen)}
+                    style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                >
+                    <View>
+                        <Image
+                            style={{ width: 120, height: 120, resizeMode: 'contain' }}
+                            source={{ uri: item.image }}
+                        />
+                        <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                        <Icon
+                            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                            name="arrowright"
+                            color="white"
+                            type='antdesign'
+                        />
+                    </View>
+                </TouchableOpacity>
+            )}
+        />
+    );
+};
+
+export default NavOptions
