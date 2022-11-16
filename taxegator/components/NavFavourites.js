fac3ad3a338e72d88095c96e558162738dd9beff
Nicholas izosimov <nicholasizosimov@Nicholass-MacBook-Pro.local>
2022-11-16 13:45:29 +0100

    Add Map Zoom & Map Favourites Component

---
+++ taxegator/components/NavFavourites.js
@@ -0,0 +1,53 @@
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Icon } from 'react-native-elements';
+import React from 'react'
+import tw from 'twrnc';
+
+
+const NavFavourites = () => {
+    const data = [
+        {
+            id: "123",
+            icon: "home",
+            location: "Home",
+            destination: "Torsgatan 29 113 21, Stockholm"
+        },
+        {
+            id: "456",
+            icon: "briefcase",
+            location: "work",
+            destination: "Lustgårdsgatan 19 112 51, Stockholm",
+        },
+    ];
+    return (
+        <View>
+            <FlatList 
+            data={data} 
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={() => (
+                <View 
+                style={[tw`bg-gray-200`, {height: 0.5}]}
+                />
+            )}
+                renderItem={({ item: { location, destination, icon }}) => (
+                    <TouchableOpacity style={tw`flex-row item-center p-5`}>
+                        <Icon
+                            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+                            name={icon}
+                            type="ionicon"
+                            color="white"
+                            size={18}
+                        />
+                        <View>
+                            <Text style={tw`font-semibold text-lg`}>{location}</Text>
+                            <Text style={tw`text-gray-500`}>{destination}</Text>
+                        </View>
+                    </TouchableOpacity>
+                )} />
+        </View>
+    )
+}
+
+export default NavFavourites
+
+const styles = StyleSheet.create({})
--- taxegator/screens/HomeScreen.js
+++ taxegator/screens/HomeScreen.js
@@ -6,6 +6,7 @@ import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplet
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from 'react-redux';
 import { setDestination, setOrigin } from "../slices/navSlice";
+import NavFavourites from '../components/NavFavourites';
 
 
 const HomeScreen = () => {
@@ -25,7 +26,7 @@ const HomeScreen = () => {
                 />
 
                 <GooglePlacesAutocomplete
-                    placeholder='Where From...'
+                    placeholder='Where From?..'
                     styles={{
                         container: {
                             flex: 0,
@@ -57,6 +58,7 @@ const HomeScreen = () => {
                 />
 
                 <NavOptions />
+                <NavFavourites />
             </View>
         </SafeAreaView>
     )
